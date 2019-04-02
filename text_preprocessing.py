40879728dbdb263145d33bea33ac40ae2f859ad5
rabialex <rabialexander@gmail.com>
2019-04-01 22:27:32 -0400

    title_change

---
+++ text_preprocessing.py
@@ -0,0 +1,12 @@
+from umap import UMAP
+from sklearn.datasets import load_digits
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+
+digits = load_digits()
+
+embedding = UMAP(n_neighbors=10, min_dist=0.001).fit_transform(digits.data)
+
