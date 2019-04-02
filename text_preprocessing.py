7270013c9825b5d1a98baa419b8ccacff47c32fb
rabialex <rabialexander@gmail.com>
2019-04-01 22:40:09 -0400

    test

--- text_preprocessing.py
+++ text_preprocessing.py
@@ -1,6 +1,7 @@
 from umap import UMAP
 from sklearn.datasets import load_digits
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -9,4 +10,4 @@ import seaborn as sns
 digits = load_digits()
 
 embedding = UMAP(n_neighbors=10, min_dist=0.001).fit_transform(digits.data)
-
+output = pd.DataFrame(embedding,columns=('x','y'))
